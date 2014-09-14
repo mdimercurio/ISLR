e690433df409208ed4e827b58fa33ba00d48fcb9
mdimercurio <matthieu.dimercurio@gmail.com>
2014-09-14 18:06:04 -0400

    Chapter 4

--- main.R
+++ main.R
@@ -143,3 +143,201 @@ plot(lm.fit1B)
 lm.fit2B = lm(y ~ x2)
 summary(lm.fit2B)
 plot(lm.fit2B)
+
+# 4.6.1 The stock market data =============================
+# p154
+names(Smarket)
+
+summary(Smarket)
+
+dim(Smarket)
+
+fix(Smarket)
+
+pairs(Smarket)
+
+cor(Smarket[,-9])
+
+attach(Smarket)
+plot(Volume)
+
+# 4.6.2 Logistic regression ===============================
+
+glm.fit = glm(Direction ~ Lag1 + Lag2 + Lag3 + Lag4 + Lag5 + Volume, family= binomial, data=Smarket)
+
+summary(glm.fit)
+
+coef(glm.fit)
+
+glm.probs = predict(glm.fit, type="response")
+head(glm.probs)
+# Use contrasts to know which response is predicted
+# Up in our case
+contrasts(Direction)
+
+## Make a prediction
+# Create a vector of 1250 elements
+glm.pred = rep("Down", 1250)
+# Predict Up if the probability is higher than 0.5
+glm.pred[glm.probs > 0.5] = "Up"
+
+# Create a confusion matrix
+table(glm.pred, Direction)
+
+mean(glm.pred == Direction)
+
+## Cross validation
+
+train = (Year<2005)
+Smarket.2005 = Smarket[!train, ]
+dim(Smarket.2005)
+Direction.2005 = Direction[!train]
+
+glm.fit = glm(Direction ~ Lag1 + Lag2 + Lag3 + Lag4 + Lag5 + Volume, family= binomial, data=Smarket, subset=train)
+glm.probs = predict(glm.fit, Smarket.2005, type="response")
+
+glm.pred = rep("Down", 252)
+glm.pred[glm.probs > .5] = "Up"
+
+table(glm.pred, Direction.2005)
+mean(glm.pred == Direction.2005)
+
+# Prediction using only Lag1 and Lag2
+glm.fit = glm(Direction ~ Lag1 + Lag2, family= binomial, data=Smarket, subset=train)
+glm.probs = predict(glm.fit, Smarket.2005, type="response")
+
+glm.pred = rep("Down", 252)
+glm.pred[glm.probs > .5] = "Up"
+
+table(glm.pred, Direction.2005)
+mean(glm.pred == Direction.2005)
+
+
+predict(glm.fit, newdata=data.frame(Lag1=c(1.2, 1.5), Lag2=c(1.1, -0.8) ), type="response")
+
+## 4.6.3 Linear discriminant analysis (LDA) ===============
+
+library(MASS)
+
+lda.fit = lda(Direction ~ Lag1 + Lag2, data=Smarket, subset=train)
+lda.fit
+
+plot(lda.fit)
+
+lda.pred = predict(lda.fit, Smarket.2005)
+names(lda.pred)
+
+lda.class = lda.pred$class
+table(lda.class, Direction.2005)
+mean(lda.class == Direction.2005)
+
+sum(lda.pred$posterior[, 1] >=.5)
+sum(lda.pred$posterior[, 1] <.5)
+
+lda.pred$posterior[1:20, 1]
+lda.class[1:20]
+# Modify the classification threshold
+sum(lda.pred$posterior[, 1] > .9)
+
+## 4.6.4 Quadratic discriminant analysis (QDA) ============
+
+qda.fit = qda(Direction ~ Lag1 + Lag2, data=Smarket, subset=train)
+
+qda.fit
+
+qda.class = predict(qda.fit, Smarket.2005)$class
+
+table(qda.class, Direction.2005)
+mean(qda.class == Direction.2005)
+
+## 4.6.5 K-nearest neighbors (KNN) ===========================
+
+library(class)
+
+train.X = cbind(Lag1, Lag2)[train,]
+test.X = cbind(Lag1, Lag2)[!train,]
+train.Direction = Direction[train]
+
+# When using KNN, R breaks the ties randomly so we need to set the seed 
+set.seed(1)
+# K = 1
+knn.pred = knn(train.X, test.X, train.Direction, k=1)
+
+table(knn.pred, Direction.2005)
+mean(knn.pred == Direction.2005)
+
+# K = 3
+knn.pred = knn(train.X, test.X, train.Direction, k=3)
+
+table(knn.pred, Direction.2005)
+mean(knn.pred == Direction.2005)
+
+## 4.6.6 [KNN] An application to caravan insurance data =========
+
+dim(Caravan)
+
+attach(Caravan)
+
+summary(Purchase)
+
+# Standardize data, using mean and stev by default
+standardized.X = scale(Caravan[,-86])
+
+var(Caravan[,1])
+var(Caravan[,2])
+
+var(standardized.X[,1])
+var(standardized.X[,2])
+
+test = 1:1000
+train.X = standardized.X[-test, ]
+test.X = standardized.X[test, ]
+train.Y = Purchase[-test]
+test.Y = Purchase[test]
+
+set.seed(1)
+# K=1
+knn.pred = knn(train.X, test.X, train.Y, k=1)
+# Comparing the knn prediction to always predicting No 
+mean(test.Y!=knn.pred)
+mean(test.Y!="No")
+# Error rate among customers that where predicted to buy insurance
+table(knn.pred, test.Y)
+9/(68+9)
+
+# K=3
+knn.pred = knn(train.X, test.X, train.Y, k=3)
+# Comparing the knn prediction to always predicting No 
+mean(test.Y!=knn.pred)
+mean(test.Y!="No")
+# Error rate among customers that where predicted to buy insurance
+table(knn.pred, test.Y)
+5/(21+5)
+
+# K=5
+knn.pred = knn(train.X, test.X, train.Y, k=5)
+# Comparing the knn prediction to always predicting No 
+mean(test.Y!=knn.pred)
+mean(test.Y!="No")
+# Error rate among customers that where predicted to buy insurance
+table(knn.pred, test.Y)
+4/(11+4)
+
+# Using logistic regression
+
+glm.fit = glm(Purchase ~., data=Caravan, family=binomial, subset=-test)
+
+glm.probs = predict(glm.fit, Caravan[test, ], type="response")
+
+# Threshold = 0.5
+glm.pred = rep("No", 1000)
+glm.pred[glm.probs > .5] = "Yes"
+table(glm.pred, test.Y)
+
+# Threshold = 0.25
+glm.pred = rep("No", 1000)
+glm.pred[glm.probs > .25] = "Yes"
+table(glm.pred, test.Y)
+11/(22+11)
+
+
