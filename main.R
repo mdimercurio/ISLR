8c4ece10bd399708130b76f85e350f1f46fabc65
mdimercurio <matthieu.dimercurio@gmail.com>
2014-09-20 23:14:09 -0400

    Chapter 4

--- main.R
+++ main.R
@@ -340,4 +340,195 @@ glm.pred[glm.probs > .25] = "Yes"
 table(glm.pred, test.Y)
 11/(22+11)
 
+## 4.7 Exercises  ======================---------
+# a
+library(ISLR)
+attach(Weekly)
+names(Weekly)
+summary(Direction)
+# b
+glm.fit = glm(Direction ~ Lag1 + Lag2 + Lag3 + Lag4 + Lag5 + Volume, family= binomial, data=Weekly)
+# c
+summary(glm.fit)
 
+glm.probs <- predict(glm.fit, type="response")
+
+glm.pred = rep("Down", nrow(Weekly))
+
+glm.pred[glm.probs > 0.5] = "Up"
+
+table(glm.pred, Direction)
+
+mean(glm.pred == Direction)
+# d
+train <- (Year<'2009')
+
+testSet <- Weekly[!train,]
+  
+glm.fit = glm(Direction ~ Lag2, family= binomial, data=Weekly, subset=train)
+
+summary(glm.fit)
+
+glm.probs <- predict(glm.fit, testSet, type="response")
+
+glm.pred = rep("Down", nrow(testSet))
+glm.pred[glm.probs > 0.5] = "Up"
+
+table(glm.pred, testSet$Direction)
+mean(glm.pred == test$Direction)
+
+# e LDA
+
+lda.fit = lda(Direction ~ Lag2, data=Weekly, subset=train)
+lda.fit
+
+plot(lda.fit)
+
+lda.pred = predict(lda.fit, testSet)
+
+table(lda.pred$class, testSet$Direction)
+mean(lda.pred$class == testSet$Direction)
+
+# f qda
+
+qda.fit = qda(Direction ~ Lag2, data=Weekly, subset=train)
+
+qda.fit
+
+qda.pred = predict(qda.fit, testSet)
+
+table(qda.pred$class, testSet$Direction)
+mean(qda.pred$class == testSet$Direction)
+
+# g KNN
+train <- Weekly$Year < 2009
+
+train.X = data.frame((Weekly$Lag2)[train])
+test.X = data.frame((Weekly$Lag2)[!train])
+train.Direction = Weekly$Direction[train]
+
+# When using KNN, R breaks the ties randomly so we need to set the seed 
+set.seed(1)
+# K = 1
+knn.pred = knn(train.X, test.X, train.Direction, k=1)
+
+table(knn.pred, testSet$Direction)
+mean(knn.pred == testSet$Direction)
+
+# 11.a
+
+names(Auto)
+
+attach(Auto)
+mpg01 <- ifelse(mpg > median(mpg), 1, 0)
+
+myAuto <- data.frame(Auto, mpg01)
+
+# 11.b
+
+
+train <- myAuto[1:300,]
+test <- myAuto[301:nrow(myAuto),]
+
+lda.fit = lda(mpg01 ~ cylinders+displacement+horsepower+weight+acceleration+year, data=train)
+
+lda.pred = predict(lda.fit, test)
+
+table(lda.pred$class, test$mpg01)
+mean(lda.pred$class == test$mpg01)
+
+qda.fit = qda(mpg01 ~ cylinders+displacement+horsepower+weight+acceleration+year, data=train)
+
+qda.pred = predict(qda.fit, test)
+
+table(qda.pred$class, test$mpg01)
+mean(qda.pred$class == test$mpg01)
+
+## 5.3.1 The validation set approach ============
+attach(Auto)
+set.seed(1)
+
+train = sample(392,196)
+
+lm.fit = lm(mpg ~ horsepower, data=Auto, subset=train)
+
+mean((mpg-predict(lm.fit, Auto))[-train]^2)
+
+lm.fit2 = lm(mpg ~ poly(horsepower, 2), data=Auto, subset=train)
+
+mean((mpg-predict(lm.fit2, Auto))[-train]^2)
+
+lm.fit3 = lm(mpg ~ poly(horsepower, 3), data=Auto, subset=train)
+
+mean((mpg-predict(lm.fit3, Auto))[-train]^2)
+
+## 5.3.2 Leave-One-Out Cross-Validation =========
+
+library(boot)
+
+glm.fit = glm(mpg ~ horsepower, data=Auto)
+
+cv.err = cv.glm(Auto, glm.fit)
+cv.err$delta
+
+cv.error = rep(0, 5)
+
+for(i in 1:5){
+  glm.fit = glm(mpg ~ poly(horsepower, i), data=Auto)
+  cv.error[i] = cv.glm(Auto, glm.fit)$delta[1]
+}
+
+cv.error
+
+## 5.3.3 k-Fold Cross-Validation ================
+
+set.seed(17)
+
+cv.error.10 = rep(0, 10)
+
+for(i in 1:10){
+  glm.fit = glm(mpg ~ poly(horsepower, i), data=Auto)
+  cv.error.10[i] = cv.glm(Auto, glm.fit, K=10)$delta[1]
+}
+
+cv.error.10
+
+## 5.3.4 The bootstrap ==========================
+
+alpha.fn = function(data, index){
+  X = data$X[index]
+  Y = data$Y[index]
+  return((var(Y)-cov(X, Y))/(var(X)+var(Y)-2*cov(X,Y)))
+}
+
+alpha.fn(Portfolio, 1:100)
+
+set.seed(1)
+
+alpha.fn(Portfolio, sample(100, 100, replace=T))
+
+boot(Portfolio, alpha.fn, R=1000)
+
+# Estimating the accuracy of a linear regression model
+
+boot.fn=function(data, index){
+  return(coef(lm(mpg~horsepower, data=data, subset=index)))
+}
+
+boot.fn(Auto, 1:392)
+
+set.seed(1)
+boot.fn(Auto, sample(392, 392, replace=T))
+
+boot(Auto, boot.fn, 1000)
+
+summary(lm(mpg~horsepower, data=Auto))$coef
+
+boot.fn=function(data, index){
+  return(coefficients(lm(mpg~horsepower+I(horsepower^2), data=data, subset=index)))
+}
+
+set.seed(1)
+boot(Auto, boot.fn, 1000)
+
+summary(lm(mpg~horsepower+I(horsepower^2), data=Auto))
